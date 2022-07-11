e9a456a4a1680b4e0bd4318db9548ae91f1eff22
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-07-12 08:46:50 +0900

    [add] 文章を動的に変更

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 1. アプリの起動
 2. ファイル構成の説明
 3. Hello Worldの文字を変えてみる(activity_main.xmlを変更する)
+4. 動的に文章を変更してみる(ViewBindingを有効化)
--- app/src/main/java/com/wsr/transferapp/MainActivity.kt
+++ app/src/main/java/com/wsr/transferapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.wsr.transferapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wsr.transferapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // activity_main.xmlに書いてあるViewを取ってきて変数に入れる
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // ↑で取ってきた内容の最上位要素を画面に表示する
+        setContentView(binding.root)
+
+        // ↑のViewの中の「text_view」のidがついている要素の文字を「Kotlin」に変更する
+        binding.textView.text = "Kotlin"
     }
 }
