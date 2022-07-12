d5efe3768cfcbc9fc61c3172dc969f2a2d0c6082
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-07-12 11:26:15 +0900

    [add] ネットワークの通信部分を記述

--- app/src/main/java/com/wsr/transferapp/MainActivity.kt
+++ app/src/main/java/com/wsr/transferapp/MainActivity.kt
@@ -3,11 +3,24 @@ package com.wsr.transferapp
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.wsr.transferapp.databinding.ActivityMainBinding
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
+    private val client = HttpClient(Android.create()) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +66,32 @@ class MainActivity : AppCompatActivity() {
 
             // EpoxyControllerに表示する文字列を通達
             mainEpoxyController.setData(viewModel.history)
+        }
+
+        // Buttonを押したときの挙動を定義
+        binding.englishButton.setOnClickListener {
+
+            // 非同期処理であるため、別の場所で実行する
+            lifecycleScope.launch {
+                // 入力欄の文字列を取得
+                val inputText = binding.editText.text
+
+                // 英語に変換
+                val translatedText = client.get("") {
+                    parameter("text", inputText)
+                    parameter("source", "ja")
+                    parameter("target", "en")
+                }.body<EnglishResponse>().text
+
+                // 変換した文字列を出力欄に代入
+                binding.textView.text = translatedText
+
+                // 履歴に文字列を追加
+                viewModel.history.add(translatedText)
+
+                // EpoxyControllerに表示する文字列を通達
+                mainEpoxyController.setData(viewModel.history)
+            }
         }
     }
 }
